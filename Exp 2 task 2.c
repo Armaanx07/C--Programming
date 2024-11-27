bb1ab1847947929d96e98637db23b1304bd8babb
Armaanx07 <armaan07@eng.rizvi.edu.in>
2024-11-27 17:53:33 +0530

    Add files via upload

---
+++ Exp 2 task 2.c
@@ -0,0 +1,34 @@
+/*Aim : WAP to find  the sum of all odd numbers entered by user
+Name : Armaan shaikh
+Roll no : 57
+UIN     : 241A066
+Division: A*/
+#include<stdio.h>
+int main()
+{
+    int sum=0, start, end, i;
+
+    printf("\t\t\t***SUM OF ODD NUMBERS***\n\n\n");
+
+    printf("Enter Starting Number=");
+    scanf("%d", &start);
+
+    printf("Enter Ending Numbers=");
+    scanf("%d", &end);
+
+    if(start>end)
+    {
+        printf("ERROR");
+    }
+    else
+    {
+        for(i=start;i<=end;i++)
+        {
+            if(i%2!=0)
+                sum=sum+i;
+        }
+    }
+
+    printf("Sum of all the Odd Numbers between %d and %d = %d", start, end, sum);
+    return 0;
+}
