d5dc8635deb938d60ad08dbafbed3951e69db396
Armaanx07 <armaan07@eng.rizvi.edu.in>
2024-11-27 16:56:43 +0530

    Add files via upload

---
+++ Exp 1  task 2.c
@@ -0,0 +1,25 @@
+/*Aim :WAP to get students PCM marks from the user and find the average. Use conditional operator to print if the student is eligible for admission. Eligibility criteria is 50% in PCM.
+Name : Armaan shaikh
+Roll no : 57
+UIN     : 241A066
+Division: A*/
+# include<stdio.h>
+int main ()
+{ int P,C,M,Avg;
+
+     printf("ENTER PHYSICS MARKS:\n");
+    scanf("%d",&P);
+
+     printf("ENTER CHEMISTRY MARKS:\n");
+    scanf("%d",&C);
+
+    printf("ENTER MATHS MARKS:\n");
+    scanf("%d",&M);
+
+    Avg = (P + C + M)/3;
+
+    printf("PCM Avg= %d",Avg);
+    Avg>=50?printf("\nYou are eligible for admission"):printf("\nYou are not eligible for admission");
+
+
+}
