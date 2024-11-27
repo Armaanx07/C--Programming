ecdc9cf17a107be5f566c42901c424dacc336de8
Armaanx07 <armaan07@eng.rizvi.edu.in>
2024-11-28 00:51:32 +0530

    Add files via upload

---
+++ Exp 10 task 1.c
@@ -0,0 +1,20 @@
+/*AIM: WAP to add two numbers using pointers.
+Name : Armaan shaikh
+Roll no : 57
+UIN     : 241A066
+Division: A*/
+#include<stdio.h>
+int main(){
+int num1, num2, sum;
+int *ptr1, *ptr2;
+printf("\t\t**** Addition using Pointers ****\n\n");
+printf ("Enter first number: ");
+scanf ("%d",&num1);
+printf ("Enter second number: ");
+scanf ("%d", &num2);
+ptr1 = &num1;
+ptr2 = &num2;
+sum = *ptr1 + *ptr2;
+printf ("Sum of %d and %d is: %d\n", num1, num2, sum);
+return 0;
+}
