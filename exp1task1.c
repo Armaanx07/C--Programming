99ce42cf57be094721d5852da259cbc7dde850ff
Armaanx07 <armaan07@eng.rizvi.edu.in>
2024-11-27 15:31:46 +0530

    Add files via upload

---
+++ exp1task1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    printf("NAME   :ARMAAN SHAIKH\n");
+    printf("AGE    :19\n");
+    printf("CLASS  :FE\n");
+    printf("DIV    :A\n");
+    printf("UIN    :241A066\n");
+       return 0;
+}
+/*
+NAME   :ARMAAN SHAIKH
+AGE    :19
+CLASS  :FE
+DIV    :A
+UIN    :241A066
+*/
